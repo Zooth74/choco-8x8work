f74ef88316277fca540748a53bc1ed25c258c45f
Kip Bowman <154554880+Zooth74@users.noreply.github.com>
2026-06-25 16:29:48 -0700

    Update 8x8Work MSI download URL and checksum
    8.34.1.11

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = '8x8Work'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://work-desktop-assets.8x8.com/prod-publish/ga/work-64-msi-v8.28.2-3.msi'
-$checksum64     = '40ecfc02fa3bb14c1c5e3ce8da7095a4b491e5be01652cfefc77595742f48aa9'
+$url64          = 'https://work-desktop-assets.8x8.com/prod-publish/ga/work-64-msi-v8.34.1-11.msi'
+$checksum64     = 'a5b4ed9ef3e00a9e41a50654a9a01e6d83c7de28ed62f01ffab61f238454b469'
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'MSI'
